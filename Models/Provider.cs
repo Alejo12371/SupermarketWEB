e3e29c8735e4b47b1b2294ccdcc6b9fe9f394d17
Alejandro Parra <xjuanchito102371x@gmail.com>
2023-11-18 22:29:25 -0500

    Modelos

---
+++ Models/Provider.cs
@@ -0,0 +1,13 @@
+﻿namespace SupermarketWEB.Models
+{
+    public class Provider
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public Product? Product { get; set; } = default!;
+    }
+}
